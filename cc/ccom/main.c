77a59422168ae43f8652092daa6a7161db94d62b
ragge <ragge>
2003-07-06 14:19:12 +0000

    Exchange upoff for something else.

--- cc/ccom/main.c
+++ cc/ccom/main.c
@@ -1,4 +1,4 @@
-/*	$Id: main.c,v 1.26 2003/07/06 13:06:34 ragge Exp $	*/
+/*	$Id: main.c,v 1.27 2003/07/06 14:19:12 ragge Exp $	*/
 
 /*
  * Copyright (c) 2002 Anders Magnusson. All rights reserved.
@@ -99,10 +99,8 @@ main(int argc, char *argv[])
 {
 
 	int ch;
-	extern unsigned int offsz;
 	extern char *release;
 
-	offsz = caloff();
 	while ((ch = getopt(argc, argv, "VlwX:Z:W:sOT:")) != -1)
 		switch (ch) {
 #if !defined(MULTIPASS) || defined(PASS1)
