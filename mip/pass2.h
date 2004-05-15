927622a9473c36b18dd881e56c189c19b3d4501f
ragge <ragge>
2004-05-15 13:44:09 +0000

    Remove a binch of unneccessary prototypes.

--- mip/pass2.h
+++ mip/pass2.h
@@ -1,4 +1,4 @@
-/*	$Id: pass2.h,v 1.35 2004/05/15 08:06:17 ragge Exp $	*/
+/*	$Id: pass2.h,v 1.36 2004/05/15 13:44:09 ragge Exp $	*/
 /*
  * Copyright(C) Caldera International Inc. 2001-2002. All rights reserved.
  *
@@ -196,7 +196,9 @@ struct optab *nxtmatch(struct optab *);
 int chkmatch(NODE *, int, int, int);
 int match(NODE *p, int cookie);
 int nmatch(NODE *p, int what);
+#ifndef special
 int special(NODE *, int);
+#endif
 int setasg(NODE *, int);
 int setuni(NODE *, int);
 int sucomp(NODE *);
