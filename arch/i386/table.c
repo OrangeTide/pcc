b0bd8203399a84e990c925d6132b270a1af40b4b
ragge <ragge>
2004-12-30 21:11:00 +0000

    Add no-op move function.

--- arch/i386/table.c
+++ arch/i386/table.c
@@ -1,4 +1,4 @@
-/*	$Id: table.c,v 1.39 2004/12/30 10:36:29 ragge Exp $	*/
+/*	$Id: table.c,v 1.40 2004/12/30 21:11:00 ragge Exp $	*/
 /*
  * Copyright (c) 2003 Anders Magnusson (ragge@ludd.luth.se).
  * All rights reserved.
@@ -691,6 +691,14 @@ struct optab table[] = {
 	SAREG|STAREG,	TCHAR|TUCHAR,
 		NAREG,	RRIGHT,
 		"	movb ZL,ZR\n" },
+
+/* needed for reg->fpreg sconv at force */
+{ MOVE,		FOREFF|INTBREG,
+	SBREG|STBREG,	TFLOAT|TDOUBLE|TLDOUBLE,
+	SBREG|STBREG,	TFLOAT|TDOUBLE|TLDOUBLE,
+		0,	RRIGHT,
+		"" },
+
 /*
  * DIV/MOD/MUL 
  */
