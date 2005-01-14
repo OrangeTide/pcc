7ccb9a79e3ef97377a7a603adc0f4f65c8f44e33
ragge <ragge>
2005-01-14 16:10:07 +0000

    Call defnam from here.

--- cc/ccom/pftn.c
+++ cc/ccom/pftn.c
@@ -1,4 +1,4 @@
-/*	$Id: pftn.c,v 1.131 2005/01/14 15:46:53 ragge Exp $	*/
+/*	$Id: pftn.c,v 1.132 2005/01/14 16:10:07 ragge Exp $	*/
 /*
  * Copyright (c) 2003 Anders Magnusson (ragge@ludd.luth.se).
  * All rights reserved.
@@ -621,6 +621,7 @@ dclargs()
 done:	cendarg();
 	setloc1(PROG);
 	defalign(ALINT);
+	defnam(cftnsp);
 	ftnno = getlab();
 	retlab = getlab();
 	send_passt(IP_PROLOG, -1, -1, DECREF(cftnsp->stype));
