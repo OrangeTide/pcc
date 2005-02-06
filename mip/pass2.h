e64d323c30db1e682d679212fba8bb2ae39b58e2
pj <pj>
2005-02-06 16:31:56 +0000

    Make computeDF work

--- mip/pass2.h
+++ mip/pass2.h
@@ -1,4 +1,4 @@
-/*	$Id: pass2.h,v 1.53 2005/02/06 16:17:28 pj Exp $	*/
+/*	$Id: pass2.h,v 1.54 2005/02/06 16:31:56 pj Exp $	*/
 /*
  * Copyright(C) Caldera International Inc. 2001-2002. All rights reserved.
  *
@@ -158,8 +158,8 @@ extern	int callflag;
 
 extern	int fregs;
 
-#define BITSET(arr, bit)  arr[bit/8] |= (1 << (bit & 7))
-#define TESTBIT(arr, bit) arr[bit/8] & (1 << (bit & 7))
+#define BITSET(arr, bit)  (arr[bit/8] |= (1 << (bit & 7)))
+#define TESTBIT(arr, bit) (arr[bit/8] & (1 << (bit & 7)))
 
 /* code tables */
 extern	struct optab {
