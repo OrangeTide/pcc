ffb34e5ee83992470f4344f9dd485292f0925ce2
ragge <ragge>
2005-06-28 19:55:05 +0000

    No need for this deflab.

--- arch/i386/local2.c
+++ arch/i386/local2.c
@@ -1,4 +1,4 @@
-/*	$Id: local2.c,v 1.46 2005/06/24 08:45:31 ragge Exp $	*/
+/*	$Id: local2.c,v 1.47 2005/06/28 19:55:05 ragge Exp $	*/
 /*
  * Copyright (c) 2003 Anders Magnusson (ragge@ludd.luth.se).
  * All rights reserved.
@@ -134,7 +134,6 @@ eoftn(struct interpass_prolog *ipp)
 		spoff = offcalc(ipp);
 
 	/* return from function code */
-	deflab(ipp->ipp_ip.ip_lbl);
 	for (i = ipp->ipp_regs; i < MAXRVAR; i++) {
 		fprintf(stdout, "	movl -%d(%s),%s\n",
 		    regoff[i-ipp->ipp_regs], rnames[FPREG], rnames[i+1]);
