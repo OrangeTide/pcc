3df3e884d2e0a2ad12ec87bbfc873b11a852c331
ragge <ragge>
2005-12-11 08:10:32 +0000

    Fix liveness calculation bug. Change ASSIGN table syntax slightly.

--- mip/regs.c
+++ mip/regs.c
@@ -1,4 +1,4 @@
-/*	$Id: regs.c,v 1.94 2005/12/08 20:10:38 ragge Exp $	*/
+/*	$Id: regs.c,v 1.95 2005/12/11 08:10:32 ragge Exp $	*/
 /*
  * Copyright (c) 2005 Anders Magnusson (ragge@ludd.luth.se).
  * All rights reserved.
@@ -129,7 +129,7 @@ int nodnum = 100;
 /* XXX */
 REGW *ablock;
 
-static int tempmin, tempmax, savregs, basetemp, dontregs;
+static int tempmin, tempmax, savregs, basetemp, dontregs, xbits;
 
 /*
  * Return the REGW struct for a temporary.
@@ -641,7 +641,7 @@ static void
 addalledges(REGW *e)
 {
 	int i, j, k;
-	int nbits = tempmax - tempmin;
+	int nbits = xbits;
 	struct lives *l;
 	void (*fun)(REGW *, REGW *);
 
@@ -975,7 +975,7 @@ LivenessAnalysis(void)
 #define	SETCOPY(t,f,i,n) for (i = 0; i < n; i += NUMBITS) t[i] = f[i]
 #define	SETSET(t,f,i,n) for (i = 0; i < n; i += NUMBITS) t[i] |= f[i]
 #define	SETCLEAR(t,f,i,n) for (i = 0; i < n; i += NUMBITS) t[i] &= ~f[i]
-#define	SETCMP(v,t,f,i,n) for (i = v = 0; i < n; i += NUMBITS) \
+#define	SETCMP(v,t,f,i,n) for (i = 0; i < n; i += NUMBITS) \
 	if (t[i] != f[i]) v = 1
 
 /*
@@ -995,7 +995,7 @@ Build(struct interpass *ipole)
 	if (xtemps) {
 		/* Just fetch space for the temporaries from stack */
 
-		nbits = tempmax - tempmin;
+		nbits = xbits;
 		gen = alloca(nbblocks*sizeof(bittype*));
 		kill = alloca(nbblocks*sizeof(bittype*));
 		in = alloca(nbblocks*sizeof(bittype*));
@@ -1831,7 +1831,8 @@ ngenregs(struct interpass *ipole)
 		dontregs |= REGBIT(FPREG);
 #endif
 
-	if ((nbits = tempmax - tempmin)) {
+	nbits = xbits = tempmax - tempmin;
+	if (nbits) {
 		nblock = tmpalloc(nbits * sizeof(REGW));
 
 		nblock -= tempmin;
