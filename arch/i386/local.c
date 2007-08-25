25413b9d485f2f23d0aedab5a6ef30cd66be7cad
ragge <ragge>
2007-08-25 05:57:49 +0000

    Dont convert addrof temps early.

--- arch/i386/local.c
+++ arch/i386/local.c
@@ -1,4 +1,4 @@
-/*	$Id: local.c,v 1.53 2007/08/19 19:25:22 ragge Exp $	*/
+/*	$Id: local.c,v 1.54 2007/08/25 05:57:49 ragge Exp $	*/
 /*
  * Copyright (c) 2003 Anders Magnusson (ragge@ludd.luth.se).
  * All rights reserved.
@@ -52,8 +52,12 @@ clocal(NODE *p)
 	register int m;
 	TWORD t;
 
-//printf("in:\n");
-//fwalk(p, eprint, 0);
+#ifdef PCC_DEBUG
+	if (xdebug) {
+		printf("clocal: %p\n", p);
+		fwalk(p, eprint, 0);
+	}
+#endif
 	switch( o = p->n_op ){
 
 	case NAME:
@@ -144,6 +148,11 @@ clocal(NODE *p)
 		/* if left is SCONV, cannot remove */
 		if (l->n_op == SCONV)
 			break;
+
+		/* avoid ADDROF TEMP */
+		if (l->n_op == ADDROF && l->n_left->n_op == TEMP)
+			break;
+
 		/* if conversion to another pointer type, just remove */
 		if (p->n_type > BTMASK && l->n_type > BTMASK)
 			goto delp;
@@ -302,9 +311,12 @@ clocal(NODE *p)
 		    CHAR, 0, MKSUE(CHAR));
 		break;
 	}
-//printf("ut:\n");
-//fwalk(p, eprint, 0);
-
+#ifdef PCC_DEBUG
+	if (xdebug) {
+		printf("clocal end: %p\n", p);
+		fwalk(p, eprint, 0);
+	}
+#endif
 	return(p);
 }
 
