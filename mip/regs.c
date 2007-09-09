d6c34f4c22d3e002e3d261cb196443928079362f
ragge <ragge>
2007-09-09 10:02:13 +0000

    Increase number of register allocation maxloops. 3 should be enough, but under some circumstances more registers are allocated second round.  This should be investigated further.

--- mip/regs.c
+++ mip/regs.c
@@ -1,4 +1,4 @@
-/*	$Id: regs.c,v 1.148 2007/08/25 05:56:22 ragge Exp $	*/
+/*	$Id: regs.c,v 1.149 2007/09/09 10:02:13 ragge Exp $	*/
 /*
  * Copyright (c) 2005 Anders Magnusson (ragge@ludd.luth.se).
  * All rights reserved.
@@ -30,7 +30,7 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define	MAXLOOP	10 /* Max number of allocation loops XXX 3 should be enough */
+#define	MAXLOOP	20 /* Max number of allocation loops XXX 3 should be enough */
 
 #define MAX(a,b) (((a) > (b)) ? (a) : (b))
  
