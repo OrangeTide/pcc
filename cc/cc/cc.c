e624ba73cdf9a58e1a85600ff9161c73d3113ec1
ragge <ragge>
2007-09-26 14:48:47 +0000

    Many patches, mostly install/docs, from Jeremy C. Reed.

--- cc/cc/cc.c
+++ cc/cc/cc.c
@@ -1,4 +1,4 @@
-/*	$Id: cc.c,v 1.60 2007/09/24 15:42:00 ragge Exp $	*/
+/*	$Id: cc.c,v 1.61 2007/09/26 14:48:47 ragge Exp $	*/
 /*
  * Copyright(C) Caldera International Inc. 2001-2002. All rights reserved.
  *
@@ -324,8 +324,8 @@ main(int argc, char *argv[])
 	/* Sanity checking */
 	if (nc == 0 && nl == 0)
 		errorx(8, "no input files");
-	if (outfile && (cflag || sflag) && nc > 1)
-		errorx(8, "-o given with -c || -S and more than one file");
+	if (outfile && (cflag || sflag || Eflag) && nc > 1)
+		errorx(8, "-o given with -c || -E || -S and more than one file");
 	if (outfile && clist[0] && strcmp(outfile, clist[0]) == 0)
 		errorx(8, "output file will be clobbered");
 #if 0
@@ -405,6 +405,8 @@ main(int argc, char *argv[])
 		av[na++] = clist[i];
 		if (!Eflag && !Mflag)
 			av[na++] = tmp4;
+		if (Eflag && outfile)
+			 av[na++] = outfile;
 		av[na++]=0;
 		if (callsys(passp, av))
 			{exfail++; eflag++;}
