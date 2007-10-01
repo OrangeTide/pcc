8f914c826eecd47b8eca36d0452a296b4c97af34
ragge <ragge>
2007-10-01 15:29:35 +0000

    Add deljumps as default optimization.

--- cc/cc/cc.c
+++ cc/cc/cc.c
@@ -1,4 +1,4 @@
-/*	$Id: cc.c,v 1.63 2007/09/29 13:22:40 ragge Exp $	*/
+/*	$Id: cc.c,v 1.64 2007/10/01 15:29:35 ragge Exp $	*/
 /*
  * Copyright(C) Caldera International Inc. 2001-2002. All rights reserved.
  *
@@ -434,6 +434,7 @@ main(int argc, char *argv[])
 			av[na++] = "-k";
 		if (Oflag) {
 			av[na++] = "-xtemps";
+			av[na++] = "-xdeljumps";
 		}
 		for (j = 0; j < xnum; j++)
 			av[na++] = xlist[j];
