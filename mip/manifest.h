ab37f30392123e3119e4cc1aac01f4bda7ffab68
ragge <ragge>
2007-11-03 11:05:34 +0000

    kflag exists in both passes.

--- mip/manifest.h
+++ mip/manifest.h
@@ -1,4 +1,4 @@
-/*	$Id: manifest.h,v 1.62 2007/10/04 16:44:32 ragge Exp $	*/
+/*	$Id: manifest.h,v 1.63 2007/11/03 11:05:34 ragge Exp $	*/
 /*
  * Copyright(C) Caldera International Inc. 2001-2002. All rights reserved.
  *
@@ -173,7 +173,7 @@
  */
 extern int bdebug, tdebug, edebug;
 extern int ddebug, xdebug, f2debug;
-extern int iTflag, oTflag;
+extern int iTflag, oTflag, kflag;
 extern int vdebug, sflag, nflag, gflag;
 extern int Wstrict_prototypes, Wmissing_prototypes, Wimplicit_int,
 	Wimplicit_function_declaration;
