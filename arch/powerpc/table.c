11f062c1959b1d7b7dd76176fe30abecfe8e4a40
stefan <stefan>
2007-11-22 21:18:12 +0000

    Fix the other rule that did not load small negative 64-bit constants correctly. The error shows up if you compile main(){long long = -1;} with -xtemps.

--- arch/powerpc/table.c
+++ arch/powerpc/table.c
@@ -1,4 +1,4 @@
-/*	$Id: table.c,v 1.8 2007/11/19 22:30:10 stefan Exp $	*/
+/*	$Id: table.c,v 1.9 2007/11/22 21:18:12 stefan Exp $	*/
 /*-
  * Copyright (c) 2007 Gregory McGarry <g.mcgarry@ieee.org>
  *
@@ -490,7 +490,7 @@ struct optab table[] = {
 	SSCON,		TANY,
 		0,	RDEST,
 		"	li AL,AR\n"
-		"	li UL,0\n", },
+		"	li UL,UR\n", },
 
 /* assign constant to register */
 { ASSIGN,	FOREFF|INAREG,
