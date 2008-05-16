cecb8f281555047f2cde0c87ebd71aded30b701f
gmcgarry <gmcgarry>
2008-05-16 02:19:33 +0000

    Cleanup register definitions.

--- arch/arm/macdefs.h
+++ arch/arm/macdefs.h
@@ -1,4 +1,4 @@
-/*	$Id: macdefs.h,v 1.10 2008/03/03 03:00:49 gmcgarry Exp $	*/
+/*	$Id: macdefs.h,v 1.11 2008/05/16 02:19:33 gmcgarry Exp $	*/
 /*
  * Copyright (c) 2003 Anders Magnusson (ragge@ludd.luth.se).
  * All rights reserved.
@@ -127,12 +127,18 @@ typedef long long OFFSZ;
 #define R8	8
 #define R9	9
 #define R10	10
+#define R11	11
+#define R12	12
+#define	R13	13
+#define R14	14
+#define R15	15
 
-#define FP	11
-#define IP	12
-#define SP	13
-#define LR	14
-#define PC	15
+#define SL	R10
+#define FP	R11
+#define IP	R12
+#define SP	R13
+#define LR	R14
+#define PC	R15
 
 #define R0R1	16
 #define R1R2	17
