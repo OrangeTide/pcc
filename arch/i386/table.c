240fa24a1706bdd4557dd39077d683174161ed6f
gmcgarry <gmcgarry>
2008-06-23 12:53:50 +0000

    Convert TPOINT to (U)CHAR also.  Needed when pointer is cast to bool.

--- arch/i386/table.c
+++ arch/i386/table.c
@@ -1,4 +1,4 @@
-/*	$Id: table.c,v 1.104 2008/05/05 21:36:06 ragge Exp $	*/
+/*	$Id: table.c,v 1.105 2008/06/23 12:53:50 gmcgarry Exp $	*/
 /*
  * Copyright (c) 2003 Anders Magnusson (ragge@ludd.luth.se).
  * All rights reserved.
@@ -229,7 +229,7 @@ struct optab table[] = {
 
 /* convert int to char. This is done when register is loaded */
 { SCONV,	INCH,
-	SAREG,	TWORD,
+	SAREG,	TWORD|TPOINT,
 	SANY,	TCHAR|TUCHAR,
 		NSPECIAL|NBREG|NBSL,	RESC1,
 		"ZM", },
