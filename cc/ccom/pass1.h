09a055a74b9a3e1cb7769b8dab0af0003e56b008
gmcgarry <gmcgarry>
2008-06-25 11:50:11 +0000

    Remove newstring() prototype; it's defined in manifest.h

--- cc/ccom/pass1.h
+++ cc/ccom/pass1.h
@@ -1,4 +1,4 @@
-/*	$Id: pass1.h,v 1.145 2008/06/20 13:19:03 ragge Exp $	*/
+/*	$Id: pass1.h,v 1.146 2008/06/25 11:50:11 gmcgarry Exp $	*/
 /*
  * Copyright(C) Caldera International Inc. 2001-2002. All rights reserved.
  *
@@ -277,7 +277,6 @@ struct symtab *lookup(char *, int);
 struct symtab *getsymtab(char *, int);
 char *addstring(char *);
 char *addname(char *);
-char *newstring(char *, int);
 void symclear(int);
 struct symtab *hide(struct symtab *);
 void soumemb(NODE *, char *, int);
