954920a71398950d9b223236d4db99bbd51bffa8
gmcgarry <gmcgarry>
2009-01-23 07:11:54 +0000

    Don't link against libc_p for profiling.  At least Ubuntu 8.03 doesn't have a separate libc for profiling.

--- os/linux/ccconfig.h
+++ os/linux/ccconfig.h
@@ -1,4 +1,4 @@
-/*	$Id: ccconfig.h,v 1.10 2009/01/23 06:01:57 gmcgarry Exp $	*/
+/*	$Id: ccconfig.h,v 1.11 2009/01/23 07:11:54 gmcgarry Exp $	*/
 
 /*
  * Copyright (c) 2004 Anders Magnusson (ragge@ludd.luth.se).
@@ -44,6 +44,7 @@
 #define CRT0FILE_PROFILE PCCLIBDIR "gcrt1.o"
 
 #define	LIBCLIBS { "-lc", "-lpcc", NULL }
+#define	LIBCLIBS_PROFILE LIBCLIBS
 
 #define STARTFILES { PCCLIBDIR "crti.o", PCCLIBDIR "crtbegin.o", NULL }
 #define	ENDFILES { PCCLIBDIR "crtn.o", PCCLIBDIR "crtend.o", NULL }
