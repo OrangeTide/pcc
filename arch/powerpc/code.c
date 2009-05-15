6d97285608d3f327717bedb6720270755cc7da14
gmcgarry <gmcgarry>
2009-05-15 06:28:11 +0000

    Update powerpc target after changes to exname() handling.

--- arch/powerpc/code.c
+++ arch/powerpc/code.c
@@ -1,4 +1,4 @@
-/*	$Id: code.c,v 1.21 2009/02/08 16:48:52 ragge Exp $	*/
+/*	$Id: code.c,v 1.22 2009/05/15 06:28:11 gmcgarry Exp $	*/
 /*
  * Copyright (c) 2003 Anders Magnusson (ragge@ludd.luth.se).
  * All rights reserved.
@@ -522,8 +522,7 @@ ejobcode(int flag )
 			if (strcmp(p->name, "mcount") == 0)
 				printf("\t.indirect_symbol %s\n", p->name);
 			else
-				printf("\t.indirect_symbol %s\n",
-				    exname(p->name));
+				printf("\t.indirect_symbol %s\n", p->name);
 			printf("\tmflr r0\n");
 			printf("\tbcl 20,31,L%s$spb\n", p->name);
 			printf("L%s$spb:\n", p->name);
@@ -540,8 +539,7 @@ ejobcode(int flag )
 			if (strcmp(p->name, "mcount") == 0)
 				printf("\t.indirect_symbol %s\n", p->name);
 			else
-				printf("\t.indirect_symbol %s\n",
-				    exname(p->name));
+				printf("\t.indirect_symbol %s\n", p->name);
 			printf("\t.long	dyld_stub_binding_helper\n");
 			printf("\t.subsections_via_symbols\n");
 		}
@@ -552,8 +550,7 @@ ejobcode(int flag )
 			if (strcmp(p->name, "mcount") == 0)
 				printf("\t.indirect_symbol %s\n", p->name);
 			else
-				printf("\t.indirect_symbol %s\n",
-				    exname(p->name));
+				printf("\t.indirect_symbol %s\n", p->name);
 			printf("\t.long 0\n");
 	        }
 
