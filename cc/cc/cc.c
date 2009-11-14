0f410a53f080a6d9f4b6d933b14552fcde609de4
ragge <ragge>
2009-11-14 16:26:10 +0000

    At last - defined __GNUC__ etc.  and also __GNUC_STDC_INLINE__.

--- cc/cc/cc.c
+++ cc/cc/cc.c
@@ -1,4 +1,4 @@
-/*	$Id: cc.c,v 1.154 2009/08/13 08:01:26 gmcgarry Exp $	*/
+/*	$Id: cc.c,v 1.155 2009/11/14 16:26:10 ragge Exp $	*/
 /*
  * Copyright(C) Caldera International Inc. 2001-2002. All rights reserved.
  *
@@ -686,6 +686,12 @@ main(int argc, char *argv[])
 		av[na++] = "-D__PCC__=" MKS(PCC_MAJOR);
 		av[na++] = "-D__PCC_MINOR__=" MKS(PCC_MINOR);
 		av[na++] = "-D__PCC_MINORMINOR__=" MKS(PCC_MINORMINOR);
+#ifdef GCC_COMPAT
+		av[na++] = "-D__GNUC__=4";
+		av[na++] = "-D__GNUC_MINOR__=3";
+		av[na++] = "-D__GNUC_PATCHLEVEL__=1";
+		av[na++] = "-D__GNUC_STDC_INLINE__=1";
+#endif
 		if (ascpp)
 			av[na++] = "-D__ASSEMBLER__";
 		if (sspflag)
