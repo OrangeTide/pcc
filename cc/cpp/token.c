38acce66d306526b5f921a14e01bf1e5b5d244ab
ragge <ragge>
2010-03-24 08:23:17 +0000

    Allow # alone on lines, as requested by Mickey.

--- cc/cpp/token.c
+++ cc/cpp/token.c
@@ -1,4 +1,4 @@
-/*	$Id: token.c,v 1.31 2010/02/25 15:49:00 ragge Exp $	*/
+/*	$Id: token.c,v 1.32 2010/03/24 08:23:17 ragge Exp $	*/
 
 /*
  * Copyright (c) 2004,2009 Anders Magnusson. All rights reserved.
@@ -1138,6 +1138,10 @@ ppdir(void)
 
 	while ((ch = inch()) == ' ' || ch == '\t')
 		;
+	if (ch == '\n') { /* empty directive */
+		unch(ch);
+		return;
+	}
 	if (ch < 'a' || ch > 'z')
 		goto out; /* something else, ignore */
 	i = 0;
