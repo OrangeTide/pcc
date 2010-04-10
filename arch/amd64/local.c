7913b514ed15dfc759ddca1fcfedb9e605b0f90d
ragge <ragge>
2010-04-10 09:40:33 +0000

    Add con/destructor support. XXX pragma init/fini should be MI.

--- arch/amd64/local.c
+++ arch/amd64/local.c
@@ -1,4 +1,4 @@
-/*	$Id: local.c,v 1.9 2010/04/07 14:45:49 ragge Exp $	*/
+/*	$Id: local.c,v 1.10 2010/04/10 09:40:33 ragge Exp $	*/
 /*
  * Copyright (c) 2008 Michael Shalayeff
  * Copyright (c) 2003 Anders Magnusson (ragge@ludd.luth.se).
@@ -1049,11 +1049,13 @@ fixdef(struct symtab *sp)
 		alias = NULL;
 	}
 	if ((constructor || destructor) && (sp->sclass != PARAM)) {
-		printf("\t.section .%ctors,\"aw\",@progbits\n",
-		    constructor ? 'c' : 'd');
-		printf("\t.p2align 2\n");
-		printf("\t.long %s\n", exname(sp->sname));
-		printf("\t.previous\n");
+		NODE *p = talloc();
+
+		p->n_op = NAME;
+		p->n_sp =
+		  (struct symtab *)(constructor ? "constructor" : "destructor");
+		sp->ssue = sueget(sp->ssue);
+		sp->ssue->suega = gcc_attr_parse(p);
 		constructor = destructor = 0;
 	}
 }
