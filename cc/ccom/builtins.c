f22302ca16175ee817e6c464d973cf066bdada20
ragge <ragge>
2011-03-03 20:31:04 +0000

    Pullup rev 1.19 & 1.20 to mainline.  Fixes Jira#PCC-270 by Chris Spiegel.

--- cc/ccom/builtins.c
+++ cc/ccom/builtins.c
@@ -1,4 +1,4 @@
-/*	$Id: builtins.c,v 1.18 2011/02/20 12:05:52 ragge Exp $	*/
+/*	$Id: builtins.c,v 1.18.2.1 2011/03/03 20:31:04 ragge Exp $	*/
 /*
  * Copyright (c) 2003 Anders Magnusson (ragge@ludd.luth.se).
  * All rights reserved.
@@ -502,6 +502,7 @@ static TWORD strcmpt[] = { CHAR|PTR, CHAR|PTR };
 static TWORD strcpyt[] = { CHAR|PTR, CHAR|PTR, INT };
 static TWORD strncpyt[] = { CHAR|PTR, CHAR|PTR, SIZET, INT };
 static TWORD strchrt[] = { CHAR|PTR, INT };
+static TWORD strcspnt[] = { CHAR|PTR, CHAR|PTR };
 static TWORD nant[] = { CHAR|PTR };
 static TWORD bitt[] = { UNSIGNED };
 static TWORD bitlt[] = { ULONG };
@@ -563,6 +564,7 @@ static const struct bitable {
 	{ "__builtin_strrchr", builtin_unimp, 2, strchrt, CHAR|PTR },
 	{ "__builtin_strncpy", builtin_unimp, 3, strncpyt, CHAR|PTR },
 	{ "__builtin_strncat", builtin_unimp, 3, strncpyt, CHAR|PTR },
+	{ "__builtin_strcspn", builtin_unimp, 2, strcspnt, SIZET },
 #ifndef TARGET_STDARGS
 	{ "__builtin_stdarg_start", builtin_stdarg_start, 2 },
 	{ "__builtin_va_start", builtin_stdarg_start, 2 },
