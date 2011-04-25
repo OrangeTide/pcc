4e141bf529c5ac45243ba31525c615b04e9ee7fd
ragge <ragge>
2011-04-25 14:53:35 +0000

    Handle struct packing correctly; never cause packing to increase alignment. Fixes Jira#PCC-332 by Iain Hibbert.

--- cc/ccom/gcc_compat.c
+++ cc/ccom/gcc_compat.c
@@ -1,4 +1,4 @@
-/*      $Id: gcc_compat.c,v 1.78 2011/04/07 18:50:16 ragge Exp $     */
+/*      $Id: gcc_compat.c,v 1.79 2011/04/25 14:53:35 ragge Exp $     */
 /*
  * Copyright (c) 2004 Anders Magnusson (ragge@ludd.luth.se).
  * All rights reserved.
@@ -469,7 +469,7 @@ gcc_tcattrfix(NODE *p)
 {
 	struct symtab *sp;
 	struct attr *ap;
-	int sz, coff, csz, al;
+	int sz, coff, csz, al, oal;
 
 	if ((ap = attr_find(p->n_ap, GCC_ATYP_PACKED)) == NULL)
 		return; /* nothing to fix */
@@ -479,13 +479,14 @@ gcc_tcattrfix(NODE *p)
 	/* Must repack struct */
 	coff = csz = 0;
 	for (sp = strmemb(ap); sp; sp = sp->snext) {
+		oal = talign(sp->stype, sp->sap);
+		if (oal > al)
+			oal = al;
 		if (sp->sclass & FIELD)
 			sz = sp->sclass&FLDSIZ;
 		else
 			sz = (int)tsize(sp->stype, sp->sdf, sp->sap);
-		SETOFF(sz, al);
-		sp->soffset = coff;
-		coff += sz;
+		sp->soffset = upoff(sz, oal, &coff);
 		if (coff > csz)
 			csz = coff;
 		if (p->n_type == UNIONTY)
