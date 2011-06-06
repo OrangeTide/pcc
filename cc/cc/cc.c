eecd46eb78213c9586dacee86a8e62b69e9ccb86
plunky <plunky>
2011-06-06 13:26:15 +0000

    const argument

--- cc/cc/cc.c
+++ cc/cc/cc.c
@@ -1,4 +1,4 @@
-/*	$Id: cc.c,v 1.182 2011/06/03 15:34:00 plunky Exp $	*/
+/*	$Id: cc.c,v 1.183 2011/06/06 13:26:15 plunky Exp $	*/
 /*
  * Copyright(C) Caldera International Inc. 2001-2002. All rights reserved.
  *
@@ -137,7 +137,7 @@ char	*tmp4;
 char	*outfile, *ermfile;
 static void add_prefix(const char *);
 static char *find_file(const char *, int);
-char *copy(char *, int);
+char *copy(const char *, int);
 char *setsuf(char *, char);
 int getsuf(char *);
 int main(int, char *[]);
@@ -1458,7 +1458,7 @@ callsys(char *f, char *v[])
  * Make a copy of string as, mallocing extra bytes in the string.
  */
 char *
-copy(char *s, int extra)
+copy(const char *s, int extra)
 {
 	int len = strlen(s)+1;
 	char *rv;
