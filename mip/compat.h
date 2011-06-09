252172bc8485488765fb35ba0c5b6c6a913066e0
plunky <plunky>
2011-06-09 19:24:46 +0000

    tidy up compat codez
    1. include "compat.h" rather than "manifest.h"; this file is
       not strictly part of the pcc compiler
    
    2. cut unused variable "start"
    
    3. include files where appropriate
        [c99] - <stddef.h> for size_t
        [c99] - <stdarg.h> for va_list
        [c99] - <string.h> for strlen()
        [c99] - <ctype.h> for isdigit()
      [posix] - <fcntl.h> for open()
      [posix] - <unistd.h> for getpid()
    
    4. prototype standardisation
      [posix] - int getopt(int, char * const [], const char *)
    
    5. function renames (gcc complains about conflicts with builtins)
        [c99] - round -> lroundl  (could use c99 <math.h> for this?)
        [gcc] - pow10 -> ldpow10

--- mip/compat.h
+++ mip/compat.h
@@ -1,4 +1,4 @@
-/*	$Id: compat.h,v 1.4 2011/06/05 08:54:43 plunky Exp $	*/
+/*	$Id: compat.h,v 1.5 2011/06/09 19:24:46 plunky Exp $	*/
 
 /*
  * Just compatibility function prototypes.
@@ -8,20 +8,20 @@
 #ifndef COMPAT_H
 #define COMPAT_H
 
-#include <string.h>
-
 #ifndef HAVE_STRLCPY
+#include <stddef.h>	/* size_t */
 size_t strlcpy(char *dst, const char *src, size_t siz);
 #endif
 
 #ifndef HAVE_STRLCAT
+#include <stddef.h>	/* size_t */
 size_t strlcat(char *dst, const char *src, size_t siz);
 #endif
 
 #ifndef HAVE_GETOPT
 extern char *optarg;
 extern int optind;
-int getopt(int, char **, char *);
+int getopt(int, char * const [], const char *);
 #endif
 
 #ifndef HAVE_BASENAME
@@ -37,10 +37,13 @@ int ffs(int);
 #endif
 
 #ifndef HAVE_SNPRINTF
+#include <stddef.h>	/* size_t */
 int snprintf(char *str, size_t count, const char *fmt, ...);
 #endif
 
 #ifndef HAVE_VSNPRINTF
+#include <stddef.h>	/* size_t */
+#include <stdarg.h>	/* va_list */
 int vsnprintf(char *str, size_t count, const char *fmt, va_list args);
 #endif
 
