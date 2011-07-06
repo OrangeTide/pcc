11341e46b5ed6adad937d2d92ea622a0fb6890e3
plunky <plunky>
2011-07-06 17:07:23 +0000

    Since -r1.17, hasgoto() is no longer needed.. remove it

--- cc/ccom/builtins.c
+++ cc/ccom/builtins.c
@@ -1,4 +1,4 @@
-/*	$Id: builtins.c,v 1.26 2011/06/05 10:19:25 ragge Exp $	*/
+/*	$Id: builtins.c,v 1.27 2011/07/06 17:07:23 plunky Exp $	*/
 /*
  * Copyright (c) 2003 Anders Magnusson (ragge@ludd.luth.se).
  * All rights reserved.
@@ -53,28 +53,6 @@ builtin_alloca(NODE *f, NODE *a, TWORD rt)
 	spalloc(t, a, SZCHAR);
 	tfree(f);
 	return u;
-}
-
-/*
- * See if there is a goto in the tree.
- * XXX this function is a hack for a flaw in handling of 
- * compound expressions and inline functions and should not be 
- * needed.
- */
-static int
-hasgoto(NODE *p)
-{
-	int o = coptype(p->n_op);
-
-	if (o == LTYPE)
-		return 0;
-	if (p->n_op == GOTO)
-		return 1;
-	if (o == UTYPE)
-		return hasgoto(p->n_left);
-	if (hasgoto(p->n_left))
-		return 1;
-	return hasgoto(p->n_right);
 }
 
 /*
