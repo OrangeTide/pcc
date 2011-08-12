e67e9ff9b96307de3c4f91e1bbe2c96e6cc58e3d
plunky <plunky>
2011-08-12 12:47:25 +0000

    use braces to mark end of long while, fix indent after

--- cc/ccom/main.c
+++ cc/ccom/main.c
@@ -1,4 +1,4 @@
-/*	$Id: main.c,v 1.110 2011/07/27 19:08:07 ragge Exp $	*/
+/*	$Id: main.c,v 1.111 2011/08/12 12:47:25 plunky Exp $	*/
 
 /*
  * Copyright (c) 2002 Anders Magnusson. All rights reserved.
@@ -146,7 +146,7 @@ main(int argc, char *argv[])
 
 	prgname = argv[0];
 
-	while ((ch = getopt(argc, argv, "OT:VW:X:Z:f:gklm:psvwx:")) != -1)
+	while ((ch = getopt(argc, argv, "OT:VW:X:Z:f:gklm:psvwx:")) != -1) {
 		switch (ch) {
 #if !defined(MULTIPASS) || defined(PASS1)
 		case 'X':
@@ -267,25 +267,26 @@ main(int argc, char *argv[])
 		default:
 			usage();
 		}
-		argc -= optind;
-		argv += optind;
+	}
+	argc -= optind;
+	argv += optind;
 
-		if (argc > 0 && strcmp(argv[0], "-") != 0) {
-			if (freopen(argv[0], "r", stdin) == NULL) {
-				fprintf(stderr, "open input file '%s':",
-				    argv[0]);
-				perror(NULL);
-				exit(1);
-			}
+	if (argc > 0 && strcmp(argv[0], "-") != 0) {
+		if (freopen(argv[0], "r", stdin) == NULL) {
+			fprintf(stderr, "open input file '%s':",
+			    argv[0]);
+			perror(NULL);
+			exit(1);
 		}
-		if (argc > 1 && strcmp(argv[1], "-") != 0) {
-			if (freopen(argv[1], "w", stdout) == NULL) {
-				fprintf(stderr, "open output file '%s':",
-				    argv[1]);
-				perror(NULL);
-				exit(1);
-			}
+	}
+	if (argc > 1 && strcmp(argv[1], "-") != 0) {
+		if (freopen(argv[1], "w", stdout) == NULL) {
+			fprintf(stderr, "open output file '%s':",
+			    argv[1]);
+			perror(NULL);
+			exit(1);
 		}
+	}
 
 	mkdope();
 	signal(SIGSEGV, segvcatch);
